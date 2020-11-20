e968c4180ac2a32dc30346d45fc7baa28fa29a70
Hassaanw27 <hassaanw27@gmail.com>
2020-11-20 01:10:31 +0100

    First commit and setup

---
+++ slothmachine/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+    <h1 className="heading"> hello world is this working</h1>
+    <input />
+    <Button>Start</Button>
+    </div>
+  );
+}
+
+export default App;
